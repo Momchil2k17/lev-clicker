91cb05f09b4afe94c1171d92f7c8c1beff4adc85
Momchil Yankov <momchil200067@gmail.com>
2024-10-14 00:35:05 +0300

    call it a day

--- script.js
+++ script.js
@@ -26,35 +26,50 @@ function updateTitle() {
     document.title = `${Math.floor(money).toLocaleString()} лева`; // Update title with current money
   }
 
+function incrementLev(){
+    money+=moneyPerClick;
+    updateMoneyDisplay();
+    }
 // Run the increment function every second
 setInterval(incrementMoney, 1000);
 setInterval(updateTitle, 1000);
 
+//buy 10 st
+let upgradeCostLev10st=10;
 
-let lev = document.querySelector(".lev-count")
-let parsedLev=parseFloat(lev.innerHTML)
+let st10Level=document.querySelector(".level-lev-10st")
+const upgradeCost10stElement = document.querySelector('.lev-cost-10st');
 
-let upgradeCostLev2=10;
+function updateUpgrade10stDisplay() {
+    upgradeCost10stElement.textContent = upgradeCostLev10st.toFixed(2);
+  }
+
+function buy10st(){
+    if(money>=upgradeCostLev10st){
+        money-=upgradeCostLev10st
+        upgradeCostLev10st*=1.15
+
+        st10Level.innerHTML++
+        moneyPerSecond += 0.1; // Increment money per second by 2
+        updateMoneyDisplay();
+        updateUpgrade10stDisplay()
+
+    }
+}
+//buy 2 leva 
+let upgradeCostLev2=100;
 
 let lev2Level=document.querySelector(".level-lev-2")
 const upgradeCostLev2Element = document.querySelector('.lev-cost-2');
-
-
 
 function updateUpgradeLev2Display() {
     upgradeCostLev2Element.textContent = upgradeCostLev2.toFixed(2);
   }
 
-
-function incrementLev(){
-    money+=moneyPerClick;
-    updateMoneyDisplay();
-    }
-
 function buy2Leva(){
     if(money>=upgradeCostLev2){
         money-=upgradeCostLev2
-        upgradeCostLev2*=1.04
+        upgradeCostLev2*=1.15
 
         lev2Level.innerHTML++
         moneyPerSecond += 2; // Increment money per second by 2
